aed122e512ab749a693a160e6119c8edbc95fba2
krishnasurya1110 <krishnasurya1110@gmail.com>
2021-12-09 08:35:51 +0530

    Updated querying for tables

--- CovidAnalysis.sql
+++ CovidAnalysis.sql
@@ -1,8 +1,16 @@
+/* Analysis - based on each country and not continent
+Query - written only for countries */ 
+
 SELECT * FROM CovidAnalysis..CovidDeaths
+WHERE continent IS NOT NULL
 ORDER BY 3, 4
 
 SELECT * FROM CovidAnalysis..CovidVaccinations
+WHERE continent IS NOT NULL
 ORDER BY 3, 4
+
+
+
 
 
 
